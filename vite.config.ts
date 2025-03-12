72e902ef979054594e7cc59e5a3a7f33891d9dd9
Boudabass <you@example.com>
2025-03-12 03:22:54 +0100

    Mise à jour de la configuration  v0.0.4

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
-    port: 8080,
+    host: "0.0.0.0",
+    port: 3000,
   },
   plugins: [
     react(),
@@ -18,5 +18,8 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: 'build',
   },
 }));
